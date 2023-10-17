dd04d150ce3f4ae9dd94867b50b6816a9de0cb8a
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-10-17 16:26:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountEmail = "rahul@gmail.com"
+var accountPass = "7654"
+accountCity = "Himachal"
+
+console.table([accountId, accountEmail, accountPass, accountCity])
+
+accountEmail = "don@google.com"
+accountPass = "rahulDon"
+accountCity = "London"
+
+console.table([accountId, accountEmail, accountPass, accountCity])
