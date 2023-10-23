a025132259ce99525a6650940ac96ded5e4df4a7
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-10-23 16:16:34 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version.
+
+// alert( 3 + 3 ) we are using node js.
+
+let name = "Rahul" // String
+let age = 21 // Integer
+let isLoggedIn = false // Boolean
+let state; // undefined
+
+//  -----< PREMITIVE DATATYPES >-----
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => 
+
+// object *Null is a object.
+
+console.log(typeof null) // --- type object.
