ca00276cc56cd3b5ee44a7cc94fad641e13befb3
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-10-23 16:53:44 +0000

    conversions and operations

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,26 @@
+// ***************** CONVERSIONS ****************
+let score = "33"
+let point = "12abc"
+// console.log(typeof score) // or console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+let anotherValue = Number(point)
+// console.log(typeof anotherValue)
+// console.log(anotherValue)
+
+// ****************** OPERATIONS *****************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello "
+let str2 = "Rahul"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+console.log(2 + 3 + "1"); 
