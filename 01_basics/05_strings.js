bad5ddf028bc3fa71e7c6b41f831970c42167ecd
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-10-24 16:42:11 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+const name = "Rahul"
+const repoCount = 2
+
+console.log(name + repoCount);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameNmae = new String('Rahul Thakur')
