5b95cff96d294f57dfbbba92c69b2a8d1bd7f0f0
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-10-25 17:16:58 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,14 @@
+const score = 400
+// let newScore = score.toString()
+// console.log(newScore.split(''))
+const balance = new Number(100)
+console.log(balance.toString())
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.4765
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// ***************** MATHS ******************88
