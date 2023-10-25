b5a0ea3c1767afd924eb6e148a87a7ad9afbb969
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-10-25 16:44:31 +0000

    Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,7 +1,30 @@
 const name = "Rahul"
 const repoCount = 2
 
-console.log(name + repoCount);
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(name + repoCount);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameNmae = new String('Rahul Thakur')
+const gameName = new String('Rahul-Thakur-maharaja')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(6));
+// console.log(gameName.indexOf('h'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-12, 7)
+// console.log(anotherString);
+
+// const newStringOne = "  Rahul  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://rahul.com/rahul%20thakur/"
+// console.log(url);
+// console.log(url.replace('%20', '-'));
+
+console.log(gameName.split('-'))
