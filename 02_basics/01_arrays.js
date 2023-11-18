c5032d9c748abe9f9323ba0f2d4a2d268ad07fb7
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-11-18 11:15:42 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,23 @@
+// arrays
+
+const myArr = [1, 2, 3, 4, 5, "Rahul", 4.5, true]
+const myHeros = ["Rahul", "Thakur"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr2[0]);  // for indexing
+console.log(typeof myArr2);
+
+// Array Methods
+// myArr.push(6)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+console.log(myArr);
