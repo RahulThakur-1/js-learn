bf2469ded0690c7e19a34184ba11cd06a681dd5c
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-11-30 09:46:51 +0000

    Array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -18,6 +18,21 @@ console.log(typeof myArr2);
 // myArr.shift()
 // console.log(myArr);
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(3));
-console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+// console.log(myArr);
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr, typeof newArr);
+
+// --- Slice , Splice --- //
+console.log("A ", myArr);
+const myn1 = myArr.slice(1,3)
+
+// console.log(myn1);
+console.log("B ", myArr)
+
+const myn2 = myArr.splice(1,3)
+console.log("c ", myArr);
+console.log(myn2);
