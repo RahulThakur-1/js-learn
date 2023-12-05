b9fac37948fb390952b58366e9893d43e0bae2d7
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-12-05 10:07:32 +0000

    Scopes in JS

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,9 +3,54 @@ let a = 300
 if (true){
     let a = 10
     const b = 20
-    console.log("Inner: ",a);
+    // console.log("Inner: ",a);
 }
 
-console.log(a);
-// console.log(b);
-// console.log(c);
+// console.log(a);
+
+
+//  {} - this is called scope in JS
+/* Means everything present in {} this is scope*/
+
+// {{...}} - this is  called nested scope ,
+// means scope under scope.
+
+function one(){
+    const username = "Rahul"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+
+        function three(){
+            const index = "hello"
+            console.log(website);
+        }
+        three()
+    }
+    
+    two()
+}
+one()
+
+if (true){
+    const username = "rahul"
+    if (username == "rahul"){
+        const website = " youtube"
+        console.log(username + website)
+    }
+    // console.log(website); -- request out of scope
+}
+// console.log(username); -- request out of scope
+
+// ------------------------- INteREstINg ---------------------------
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
