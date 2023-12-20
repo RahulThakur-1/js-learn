9aeaa87bcfff9d5b1b32f944ebc12d385ad3af0b
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-12-20 13:59:19 +0000

    Switch (other type of if else)

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "jan"
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case 2:
+        console.log("Februry");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case");
+        break;
+}
