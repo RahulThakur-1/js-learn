bfd11ed35d5623704d2dd8d1736219f976d6a56b
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-12-20 13:06:24 +0000

    IF-ELSE

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,9 +1,25 @@
 //  If 
-const user = true
-const temp = 51
+// const user = true
+// const temp = 51
 
-if (temp <= 50) {
-    console.log("Low Temperature");
-} else{
-    console.log("High Temperature");
-}
+// if (temp <= 50) {
+//     console.log("Low Temperature");
+// } else{
+//     console.log("High Temperature");
+// }
+
+// const user1 = true
+// const user2 = true
+// const user3 = false
+
+// if (user1 && user2 && 2===2){
+//     console.log("Allow to buy course.");
+// }  // And (&&) operator - all the conditions mus tbe true
+
+// else if(user1 || user3){
+//     console.log("Credit score is low.");
+// }  // OR (||) operator - either condition is true
+
+// else{
+//     console.log("Not allowed");
+// }
