11b996124212f1e53548cc1f172f61cca35358ad
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-12-22 11:38:39 +0000

    for loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,5 +1,17 @@
 // for loop
 
+// for (let index = 1; index <= 20; index++) {    
+//     if (index == 5){
+//         console.log(`Detected 5`);
+//         break 
+//     }
+//     console.log(`Value of index is ${index}`);
+// }
+
 for (let index = 1; index <= 20; index++) {    
+    if (index == 5){
+        console.log(`Detected 5`);
+        continue 
+    }
     console.log(`Value of index is ${index}`);
 }
