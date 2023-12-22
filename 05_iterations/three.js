f2515b667c3f9a4bba28d492f0e4d22c48dec029
Rahul Thakur <87636664+RahulThakur-1@users.noreply.github.com>
2023-12-22 14:03:22 +0000

    ForOf loop and MAP

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,7 +1,39 @@
 // for of loop
 
-const arr = [1, 2, 3, 4, 5]
+// const arr = [1, 2, 3, 4, 5]
 
-for (const iterator of object) {
-    
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+const greetings = "Hello World!"
+
+for (const greet of greetings) {
+    if (greet == " "){            // ---- This thing removes space from outcome
+        continue
+    }
+    // console.log(`Each char is ${greet}`);
 }
+
+// MAPS
+
+const map = new Map()
+map.set('IN', "India")
+map.set('US', "United states of America")
+map.set('SA', "South Africa")
+
+// console.log(map);
+
+for (const [key, value] of map){
+    // console.log(key, ':-', value);
+}
+
+const myObj = {
+    'game1': 'NFS',
+    'game2': 'PUBG',
+    'game3': 'Spiderman',
+}
+
+// for (const [key, value] of myObj){
+//     console.log(key,value);
+// }
